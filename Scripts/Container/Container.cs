d38b40edaebd370a59e54e968baa070afae08a3f
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2024-08-12 15:37:41 -0600

    Chest animation fixed

--- Scripts/Container/Container.cs
+++ Scripts/Container/Container.cs
@@ -6,18 +6,17 @@ public partial class Container : Interactable
     [Export] public string item;
     [Export] public int quantity;
 
-    private AnimationTree animationTree;
+    [Export] private AnimationTree animationTree;
     private bool inspected = false;
 
-    // Define the signal
-    [Signal]
-    public delegate void ItemInteractedEventHandler(string itemName, int quantity);
+
+    [Signal] public delegate void ItemInteractedEventHandler(string itemName, int quantity);
 
     public override void _Ready()
     {
         animationTree = GetNode<AnimationTree>("AnimationTree");
         animationTree.Set("active", true);
-        AddToGroup("Containers"); // Add this Container to the "containers" group
+        AddToGroup("Containers");
     }
 
     public override void Interact()
