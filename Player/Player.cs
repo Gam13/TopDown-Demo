17b0db9805e757dc8515119f6fb1dd59fb714caf
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2024-08-17 16:53:24 -0300

    Starting interact system

--- Player/Player.cs
+++ Player/Player.cs
@@ -4,6 +4,7 @@ using System;
 public partial class Player : CharacterBody2D
 {
     [Export]public PlayerData data;
+    [Export]private RayCast2D interaction;
 	private Vector2 direction = Vector2.Zero;
 	public const float Speed = 300.0f;
 
@@ -13,6 +14,7 @@ public partial class Player : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		Moviment();
+        UpdateInteractionRaycast();
 	}
 
 	public void Moviment()
@@ -24,6 +26,14 @@ public partial class Player : CharacterBody2D
 
         Velocity = velocity;
         MoveAndSlide();
+    }
+
+    private void UpdateInteractionRaycast() 
+    {
+        if (direction != Vector2.Zero)
+        {
+            interaction.TargetPosition = direction * 25;
+        }
     }
 
 
