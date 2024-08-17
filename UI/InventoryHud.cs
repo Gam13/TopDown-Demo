7141aa9cbaf2344a15705572c675f4426d827ede
João Victor de Carvalho Nobre <65377921+Gam13@users.noreply.github.com>
2024-08-17 02:38:20 -0300

    Inventory Development

--- UI/InventoryHud.cs
+++ UI/InventoryHud.cs
@@ -6,6 +6,9 @@ public partial class InventoryHud : Control
 	VBoxContainer Items;
 	VBoxContainer Weapons;
 	VBoxContainer Consumibles;
+	PackedScene Button;
+
+
 
 	[Export] PlayerData data;
 	public override void _Ready()
@@ -13,6 +16,7 @@ public partial class InventoryHud : Control
 		Items = GetNode<VBoxContainer>("MarginContainer/TabContainer/Itens");
 		Weapons = GetNode<VBoxContainer>("MarginContainer/TabContainer/Armas");
 		Consumibles = GetNode<VBoxContainer>("MarginContainer/TabContainer/Cura");
+		Button = GD.Load<PackedScene>("res://Components/Button/button_component.tscn");
 	}
 
 }
